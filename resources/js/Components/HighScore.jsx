c6a234cd42ee2bff732e518d7dd4202b7c2f950c
lmoboy <steamgoyu19@gmail.com>
2024-09-18 20:29:19 +0300

    Vlada uzdevums :D

---
+++ resources/js/Components/HighScore.jsx
@@ -0,0 +1,19 @@
+//vlada uzdevums
+/*
+1. Saņemt datus no datubāzes kad kevins beidzot izies no kovida komas (pagaidam taisi savu sarakstu)
+2. Izvadit top 10 cilvēkus
+3. Vajag izvadīt vārdus un cik tika uzminēts pēc kārtas (mūsu gadijumā no 1 līdz 50)
+*/
+
+const piemers = [
+    { username: "lolik", score: 50 },
+    { username: "oranges", score: 10 },
+    { username: "koks", score: 40 },
+    { username: "bananas", score: 20 },
+    { username: "toliks", score: 30 },
+];
+
+/*
+izmanto šo lai testēt highscore tabulu
+
+*/
