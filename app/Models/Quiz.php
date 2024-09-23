631dcdd2a8103ed56de97f0577bc37a136043da6
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-09-23 14:49:58 +0300

    added stuff to the backend

---
+++ app/Models/Quiz.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class quiz extends Model
+{
+    use HasFactory;
+
+    protected $table = 'quizzes';
+
+    protected $fillable = [
+        'owner_id',
+        'qestion',
+        'completione_count',
+        'answers',
+        'correct_answer',
+    ];
+
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Quiz;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -26,6 +27,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+
+    Route::get('/quiz/index', [Quiz::class, 'index']); //Inertia::render('Dashboard')
+    Route::get('/quiz/{id}', [Quiz::class, 'show']); //Inertia::render('Dashboard')
+    Route::post('/quiz/create', [Quiz::class, 'create']); 
+    Route::get('/quiz/new', [Quiz::class, 'new']); //Inertia::render('Dashboard')
+    Route::get('/quiz/{id}/edit', [Quiz::class, 'edit']); //Inertia::render('Dashboard')
+    Route::put('/quiz/{id]', [Quiz::class, 'update']);
+    Route::delete('/quiz/{id}', [Quiz::class, 'destroy']);
+
 });
 
 require __DIR__.'/auth.php';
