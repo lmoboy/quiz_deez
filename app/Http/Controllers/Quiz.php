bb9bcc63f8f46e17f6624359ec4b41aa171bc7cc
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-09-23 15:13:44 +0300

    made Quiz conntroller

--- app/Http/Controllers/Quiz.php
+++ app/Http/Controllers/Quiz.php
@@ -3,33 +3,40 @@
 namespace App\Http\Controllers;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use App\Models\Quiz;
 
-abstract class Quiz
+abstract class QuizControllers
 {
     public function index() {
-        return Inertia::render('Dashboard');
+        return Inertia::render('Dashboard' , ['quzzes' => [Quiz::all() ?? []]]);
     }
 
-    public function show() {
-        return Inertia::render('Dashboard');
+    public function show($id) {
+        return Inertia::render('Quiz/show' , ['quiz' => [Quiz::findorfail($id) ?? []]]);
     }
 
     public function create(Request $request) {
         $request->validate([
-            'id' => 'required',
             'owner_id' => 'required',
             'qestion' => 'required',
             'answers' => 'required',
             'correct_answer' => 'required',
         ]);
+
+        $quiz = new Quiz();
+        $quiz->owner_id = $request->owner_id;
+        $quiz->qestion = $request->qestion;
+        $quiz->answers = $request->answers;
+        $quiz->correct_answer = $request->correct_answer;
+        $quiz->save();
     }
 
     public function new() {
-        return Inertia::render('Dashboard');
+        return Inertia::render('Quiz/create');
     }
 
     public function edit() {
-        return Inertia::render('Dashboard');
+        return Inertia::render('Quiz/edit');
     }
 
     public function update(Request $request) {
@@ -40,6 +47,13 @@ abstract class Quiz
             'answers' => 'required',
             'correct_answer' => 'required',
         ]);
+
+        $quiz = Quiz::findorfail($request->id);
+        $quiz->qestion = $request->qestion;
+        $quiz->answers = $request->answers;
+        $quiz->correct_answer = $request->correct_answer;
+        $quiz->save();
+
     }
 
     public function destroy(Request $request) {
@@ -47,6 +61,11 @@ abstract class Quiz
             'id' => 'required',
             'owner_id' => 'required',
         ]);
+        $quiz = Quiz::findorfail($request->id);
+        if($quiz->owner_id !== $request->owner_id) {
+            //TODO redirect to error page
+        }
+        $quiz->delete();
     }
 
 }
