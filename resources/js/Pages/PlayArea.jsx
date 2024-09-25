df1bec31a3c9e79a544148a3b16fef4fb2aa2557
lmoboy <steamgoyu19@gmail.com>
2024-09-25 08:49:55 +0300

    test

--- resources/js/Pages/PlayArea.jsx
+++ resources/js/Pages/PlayArea.jsx
@@ -3,40 +3,93 @@ import { Head } from "@inertiajs/react";
 import { useEffect, useState } from "react";
 import QuizCard from "@/Components/QuizCard";
 function PlayArea(...prop) {
-    const [quizzes, setQuizzes] = useState([]);
-    fetch('/api_quiz/2', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    }).then((res)=>res.json()).then((data)=>console.log(data))
-    useEffect(() => {
-        const fetchQuizzes = async () => {
-          try {
-            const response = await axios.get('/quiz/index');
-            setQuizzes(response.data);
+  const [questions, setQuestions] = useState([]);
 
-          } catch (error) {
-            console.error(error);
-          }
-        };
+    // const [quizzes, setQuizzes] = useState([]);
+    // fetch('/api_quiz/2', {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    // }).then((res)=>res.json()).then((data)=>console.log(data))
+    // useEffect(() => {
+    //     const fetchQuizzes = async () => {
+    //       try {
+    //         const response = await axios.get('/quiz/index');
+    //         setQuizzes(response.data);
+
+    //       } catch (error) {
+    //         console.error(error);
+    //       }
+    //     };
     
-        fetchQuizzes();
-      }, []);
+    //     fetchQuizzes();
+    //   }, []);
+
+    // return (
+    //     <>
+    //         <NavLink href="/dashboard">DashBoard</NavLink>
+    //         <Head title="Play Area" />
+    //         <div>
+    //             {/* <QuizCard quiz={quizzes}/> */}
+    //             {quizzes.map((quiz, index) => (
+    //                 // <div dangerouslySetInnerHTML={quiz}></div>
+    //                 <QuizCard key={index} quiz={quiz} />
+    //             ))}
+    //         </div>
+    //     </>
+    // );
 
     return (
-        <>
-            <NavLink href="/dashboard">DashBoard</NavLink>
-            <Head title="Play Area" />
-            <div>
-                {/* <QuizCard quiz={quizzes}/> */}
-                {quizzes.map((quiz, index) => (
-                    // <div dangerouslySetInnerHTML={quiz}></div>
-                    <QuizCard key={index} quiz={quiz} />
-                ))}
-            </div>
-        </>
-    );
+      <>
+        <NavLink href="/dashboard">DashBoard</NavLink>
+        <form onSubmit={async (e) => {
+          e.preventDefault();
+          const question = e.target.question.value;
+          const correct = e.target.correct_answer.value;
+          const incorrect = e.target.incorrect_answers.value.split(',').filter(Boolean);
+          const response = await fetch('/api_quiz', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ question, correct, incorrect })
+          });
+          if (response.ok) {
+            setQuestions([...questions, { question, correct, incorrect }]);
+            e.target.reset();
+          } else {
+            console.error(await response.text());
+          }
+        }}>
+          <label>
+            Question:
+            <input type="text" name="question" />
+          </label>
+          <br />
+          <label>
+            Correct answer:
+            <input type="text" name="correct_answer" />
+          </label>
+          <br />
+          <label>
+            Incorrect answers (comma separated):
+            <input type="text" name="incorrect_answers" />
+          </label>
+          <br />
+          <button type="submit">Create Quiz</button>
+        </form>
+      <ul>
+        {questions.map((question, index) => (
+          <li key={index}>{question.question}</li>
+        ))}
+      </ul>
+      
+      </>
+    )
+
+
+
 }
 
 export default PlayArea;
